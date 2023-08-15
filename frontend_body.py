17d0127d3d32b5d4256cdc427be375b4ead08607
nachtigall88 <ud1narty0m123@gmail.com>
2023-08-15 18:25:20 +0300

    frontend

--- frontend_body.py
+++ frontend_body.py
@@ -2,6 +2,19 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+def rule_entries():
+    """
+    функція для ввімкнення/вимкнення ентрі-полів для вводу інформації ""
+    :return:
+    """
+    for i in [art_entry, word_entry, transl_entry]:
+        if varint.get() == 1:
+            i.config(state='normal')
+        else:
+            i.config(state='disabled')
+
+
 root = tk.Tk()
 root.title('DEUTSCHE WORTER')
 root.geometry('800x500+60+20')
@@ -16,16 +29,23 @@ click_button.grid(row=2, column=2)
 hint_combobox = ttk.Combobox(root, values=[' ', 'article', 'my hint'])
 hint_combobox.current(0)
 hint_combobox.grid(row=3, column=1)
-frame = tk.LabelFrame(root, text='isert data', bg='yellow')
-frame.grid(row=6, column=1)
-art_entry = tk.Entry(frame, width=20)
-art_entry.grid(row=0,column=1)
-word_entry = tk.Entry(frame, width=10)
-word_entry.grid(row=0,column=2)
-transl_entry = tk.Entry(frame, width=10)
-transl_entry.grid(row=0,column=3)
-insert_button = tk.Button(frame, text='INSERT',bg='black',fg='yellow')
+frame = tk.LabelFrame(root, text='isert data', fg='yellow', bg='black')
+frame.grid(row=7, column=1)
+empty_label = tk.Label(text='empty data', bg='black', fg='black', height=5)
+empty_label.grid(row=6, column=1)
+art_entry = tk.Entry(frame, width=9, state='disabled')
+art_entry.grid(row=0, column=1)
+word_entry = tk.Entry(frame, width=20, state='disabled')
+word_entry.grid(row=0, column=2)
+transl_entry = tk.Entry(frame, width=20, state='disabled')
+transl_entry.grid(row=0, column=3)
+insert_button = tk.Button(frame, text='INSERT', bg='black', fg='yellow')
 insert_button.grid(row=6, column=3)
+varint = tk.IntVar()
+enabler = tk.Checkbutton(root, text='enable/disable', bg='black', fg='red', activebackground='black',
+                         activeforeground='red', offvalue=0, onvalue=1, variable=varint, command=rule_entries)
+
+enabler.grid(row=7, column=2)
 
 if __name__ == '__main__':
     root.mainloop()
